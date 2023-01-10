332bd726265ea246da689cfbf25af7b3169058b6
Ekemwhite <ekemwhite@gmail.com>
2023-01-10 15:15:25 -0800

    Modules commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == "__main__":
+    import sys
+
+    num = 0
+
+    for i in range(1, len(sys.argv)):
+        num += int(sys.argv[i])
+    print("{}".format(num))
